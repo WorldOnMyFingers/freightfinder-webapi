ba629cc4012b5bd206ce8381a86ac350e9238f31
Emre Babayigit <emre.babayigt@gmail.com>
2020-09-23 10:19:45 +0100

    added nswag.aspnet.owin

--- FreightFinder/WebApiFreightFinder/Startup.cs
+++ FreightFinder/WebApiFreightFinder/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using NSwag.AspNet.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(WebApiFreightFinder.Startup))]
@@ -12,7 +16,30 @@ namespace WebApiFreightFinder
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            // from https://github.com/NSwag/NSwag/wiki/Middlewares			
+            var config = new HttpConfiguration();
+
+            app.UseSwaggerUi(typeof(Startup).Assembly);
+            app.UseWebApi(config);
+
+            config.MapHttpAttributeRoutes();
+            config.EnsureInitialized();
+
+            // to serve static files, see: http://stackoverflow.com/a/36297940/445927
+            string root = AppDomain.CurrentDomain.BaseDirectory;
+            //var physicalFileSystem = new PhysicalFileSystem(Path.Combine(root, "wwwroot"));
+            var physicalFileSystem = new PhysicalFileSystem(root);
+            var options = new FileServerOptions
+            {
+                RequestPath = PathString.Empty,
+                EnableDefaultFiles = true,
+                FileSystem = physicalFileSystem
+            };
+            options.StaticFileOptions.FileSystem = physicalFileSystem;
+            options.StaticFileOptions.ServeUnknownFileTypes = false;
+            app.UseFileServer(options);
 
         }
     }
